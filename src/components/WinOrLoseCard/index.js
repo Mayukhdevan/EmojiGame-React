a927c720b3ee87bc7f837d64d795d247fe93cdde
Mayukh Devan Sahu <94803484+Mayukhdevan@users.noreply.github.com>
2023-01-30 21:03:30 +0530

    Fixed score and best score

--- src/components/WinOrLoseCard/index.js
+++ src/components/WinOrLoseCard/index.js
@@ -1,38 +1,41 @@
 import './index.css'
 
 const WinOrLoseCard = props => {
-  const {result, score, bestScore, newGame} = props
+  const {result, score, newGame} = props
 
   const restartGame = () => newGame()
 
   let cardImg
   let resultText
   let scoreType
-  let scorePoints
 
   if (result === true) {
     cardImg = 'https://assets.ccbp.in/frontend/react-js/won-game-img.png'
     resultText = 'Won'
     scoreType = 'Best Score'
-    scorePoints = bestScore
   } else {
     cardImg = 'https://assets.ccbp.in/frontend/react-js/lose-game-img.png'
     resultText = 'Lose'
     scoreType = 'Score'
-    scorePoints = score
   }
 
   return (
     <div className="result-card">
-      <img className="result-image" src={cardImg} alt="Result" />
-      <p className="result-text">You {resultText}</p>
-      <p className="score-type">{scoreType}</p>
-      <p className="score-points">{scorePoints}/12</p>
-      <button className="play-again-btn" type="button" onClick={restartGame}>
-        Play Again
-      </button>
+      <img className="result-image" src={cardImg} alt="win or lose" />
+      <div className="result-card-text-wrapper">
+        <h1 className="result-text">You {resultText}</h1>
+        <p className="score-type">{scoreType}</p>
+        <p className="score-points">{score}/12</p>
+        <button className="play-again-btn" type="button" onClick={restartGame}>
+          Play Again
+        </button>
+      </div>
     </div>
   )
+}
+
+export default WinOrLoseCard
+
 }
 
 export default WinOrLoseCard
